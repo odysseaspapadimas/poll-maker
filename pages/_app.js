32a35f55b490fe3b2853464e235a07a200e5b59b
odysseaspapadimas <odysseas.patra@gmail.com>
2021-10-03 22:07:13 +0300

    fingerprint js test

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,9 @@
 import "tailwindcss/tailwind.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { Global } from "@emotion/react";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
+
 
 function MyApp({ Component, pageProps }) {
-  const breakpoints = createBreakpoints({
-    sm: "30em",
-    md: "48em",
-    lg: "62em",
-    xl: "80em",
-    "2xl": "96em",
-  });
-
-  const theme = extendTheme({ breakpoints });
-
+  
   return (
     <ChakraProvider>
       <Component {...pageProps} />
