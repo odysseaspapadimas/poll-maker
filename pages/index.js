d5de6914b150f530e4817f79d57f456127be82d3
odysseaspapadimas <odysseas.patra@gmail.com>
2021-10-03 22:20:11 +0300

    fingerprint nvm

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 import Poll from "../components/Poll/Poll";
-import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
 
 export default function Home() {
-  FingerprintJS.load({ token: process.env.PUBLIC_NEXT_FINGERPRINT_TOKEN })
-    .then((fp) => fp.get())
-    .then((result) => console.log(result.visitorId));
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
